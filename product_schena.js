3d346756ac1fcc5bc7092104257caee7039643d4
Caio Johnston <94867246+CaioJohnston@users.noreply.github.com>
2024-05-09 01:41:06 -0300

    Create product_schena.js

---
+++ product_schena.js
@@ -0,0 +1,65 @@
+const ProductSchema = new mongoose.Schema({
+  id: {
+    type: Number,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  new_price: {
+    type: Number,
+    required: true,
+  },
+  old_price: {
+    type: Number
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  height: {
+    type: Number,
+    required: true,
+  },
+  width: {
+    type: Number,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+  available: {
+    type: Boolean,
+    default: true,
+  },
+  emailID: {
+    type: String,
+    required: true,
+    validate: {
+      validator: function(v) {
+        return /\S+@\S+\.\S+/.test(v);
+      },
+      message: props => `${props.value} is not a valid email!`
+    },
+  },
+  sellerName: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model('Product', ProductSchema);
