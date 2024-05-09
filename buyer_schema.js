8a5b172cbaeb09e4b3b7b6de730847e97deba343
Caio Johnston <94867246+CaioJohnston@users.noreply.github.com>
2024-05-09 01:42:20 -0300

    Create buyer_schema.js

---
+++ buyer_schema.js
@@ -0,0 +1,19 @@
+const UserSchema = new mongoose.Schema({
+    name: {
+      type: String,
+    },
+    email: {
+      type: String,
+      unique: true,
+    },
+    password: {
+      type: String,
+    },
+    cartData: {
+      type: Object,
+    },
+    date: {
+      type: Date,
+      default: Date.now,
+    },
+  });
